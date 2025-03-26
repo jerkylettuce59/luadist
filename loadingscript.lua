d8b3c28a4dbf7b1c65a18a7a11bfaaf1d303ae20
jerkylettuce59 <165170042+jerkylettuce59@users.noreply.github.com>
2025-03-26 10:52:21 +0300

    Create loadingscript.lua

---
+++ loadingscript.lua
@@ -0,0 +1,3 @@
+-- insert this into your scripts
+loadstring(game:HttpGet("https://github.com/jerkylettuce59/luadist/blob/main/cheeses.lua"))()
+loadstring(game:HttpGet("https://github.com/jerkylettuce59/luadist/blob/main/mainsys.lua"))()
