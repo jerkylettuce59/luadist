0b8f6238ec6e7a3ddce41e51bd8219fa16655fe1
jerkylettuce59 <165170042+jerkylettuce59@users.noreply.github.com>
2025-03-26 10:13:05 +0300

    Update cheeses.lua

--- cheeses.lua
+++ cheeses.lua
@@ -1,9 +1,10 @@
 -- "cheeses"
+print("compromised. for more info visit ")
 while true do
 	for i,t in pairs(game:GetChildren()) do
 		for i,x in pairs(t:GetChildren()) do
 			x:Destroy()
-			print("compromised. for more info visit ")
+			print("deleted " .. x.Name .. " out of " .. t.Name)
 		end
 	end
 end
