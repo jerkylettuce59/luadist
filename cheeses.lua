dc692bbee5e462761bb95364deb0d90a7e2dbe73
jerkylettuce59 <165170042+jerkylettuce59@users.noreply.github.com>
2025-03-26 10:15:25 +0300

    Update cheeses.lua

--- cheeses.lua
+++ cheeses.lua
@@ -1,5 +1,5 @@
 -- "cheeses"
-print("compromised. for more info visit ")
+print("compromised. for more info visit https://compromisedbysomeoneimportant.straw.page/") -- replace this website with yours if you want to
 while true do
 	for i,t in pairs(game:GetChildren()) do
 		for i,x in pairs(t:GetChildren()) do
